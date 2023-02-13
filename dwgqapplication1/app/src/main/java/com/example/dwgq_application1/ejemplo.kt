34250bd5a9b5e147b11648f90cf9ebceada0dc4b
Daniel Garrido <daniel.garrido@epn.edu.ec>
2023-02-13 18:38:28 -0500

    Creacion proyecto goodRead
    Se inicia el deber

---
+++ dwgqapplication1/app/src/main/java/com/example/dwgq_application1/ejemplo.kt
@@ -0,0 +1,11 @@
+package com.example.dwgq_application1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ejemplo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejemplo)
+    }
+}
